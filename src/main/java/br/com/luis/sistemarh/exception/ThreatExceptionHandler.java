aa4b8badf9f51538c42985bca1ebb2acf0c65b04
Luis Henrique <luishenrique2859@hotmail.com>
2024-01-08 13:26:50 -0300

    feat: implements handleMethodArgumentNotValid

--- src/main/java/br/com/luis/sistemarh/exception/ThreatExceptionHandler.java
+++ src/main/java/br/com/luis/sistemarh/exception/ThreatExceptionHandler.java
@@ -2,17 +2,20 @@ package br.com.luis.sistemarh.exception;
 
 
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.List;
 
 
 @RestControllerAdvice
-public class ThreatExceptionHandler extends ResponseEntityExceptionHandler {
+public class ThreatExceptionHandler {
 
 
     @ExceptionHandler(IdNotFoundException.class)
@@ -22,8 +25,26 @@ public class ThreatExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<List<ValidationError>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<ValidationError> errors = ex.getFieldErrors()
+                .stream()
+                .map(ValidationError::new)
+                .toList();
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 
     public static record ErrorResponse(String message) {}
+
+
+    public record ValidationError(String field, String message) {
+        public ValidationError(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
 
 
 }
